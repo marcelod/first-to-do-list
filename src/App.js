61cf5240282d2b27e53677dae62f0499bdd4b20d
Marcelo Diniz <mdiniz@meeta.com.br>
2022-07-06 18:16:20 -0300

    estilisando components

--- src/App.js
+++ src/App.js
@@ -1,16 +1,18 @@
-import { Component } from "react";
-import FormularioCadastro from "./components/FormularioCadastro";
-import ListaDeNotas from "./components/ListaDeNotas";
+import { Component } from 'react';
+import FormularioCadastro from './components/FormularioCadastro';
+import ListaDeNotas from './components/ListaDeNotas';
+import './assets/css/App.css';
+import './assets/css/index.css';
 
 class App extends Component {
-  render () {
-    return (
-      <section>
-        <FormularioCadastro /> 
-        <ListaDeNotas />
-      </section>
-    );
-  }  
+    render() {
+        return (
+            <section className="conteudo">
+                <FormularioCadastro />
+                <ListaDeNotas />
+            </section>
+        );
+    }
 }
 
 export default App;
