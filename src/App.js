9046c941dd903f69fab285d1b69d5ed55f53c8c0
Marcelo Diniz <mdiniz@meeta.com.br>
2022-07-07 17:36:33 -0300

    criado props e state para titulo e texto

--- src/App.js
+++ src/App.js
@@ -5,11 +5,27 @@ import './assets/css/App.css';
 import './assets/css/index.css';
 
 class App extends Component {
+    constructor() {
+        super();
+        this.state = {
+            notas: [],
+        };
+    }
+
+    criarNota(titulo, texto) {
+        const novaNota = { titulo, texto };
+        const novoArrayNotas = [...this.state.notas, novaNota];
+        const novoEstado = {
+            notas: novoArrayNotas,
+        };
+        this.setState(novoEstado);
+    }
+
     render() {
         return (
             <section className="conteudo">
-                <FormularioCadastro />
-                <ListaDeNotas />
+                <FormularioCadastro criarNota={this.criarNota.bind(this)} />
+                <ListaDeNotas notas={this.state.notas} />
             </section>
         );
     }
